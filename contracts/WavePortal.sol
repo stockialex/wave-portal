d033fa5461f48338bcc1e0f31954e1f0b9c4d092
alexstocki <alexstocki22@gmail.com>
2021-11-29 09:02:54 -0300

    first block created

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,18 +6,26 @@ import "hardhat/console.sol";
 contract WavePortal {
 
     uint256 totalWaves;
+    address[] addressList;
 
     constructor() {
-        console.log("En la calle me conocen como el hip-hop, the hit, the real dance crip");
+        console.log("Start of the contract");
     }
 
     function wave() public {
         totalWaves = totalWaves + 1;
         console.log("%s has waved to you", msg.sender);
+        addressList.push(msg.sender);
     }
 
     function getTotalWaves() public view returns(uint256) {
         console.log("You get a total of %s waves", totalWaves);
         return totalWaves;
+    }
+
+    function printAddresses() public view {
+        for(uint i = 0; i < addressList.length; i++) {
+            console.log("%i) %s", i, addressList[i]);
+        }
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -4,16 +4,23 @@ const main = async () => {
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
 
-  console.log("Contract deployed to:", waveContract.address);
-  console.log("Contract deployed by:", owner.address);
+  console.log('Contract deployed to:', waveContract.address);
+  console.log('Contract deployed by:', owner.address);
 
   let waveCount;
   waveCount = await waveContract.getTotalWaves();
-  
+
   let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+  
+  waveCount = await waveContract.getTotalWaves();
+  
+  waveTxn = await waveContract.connect(randomPerson).wave();
   await waveTxn.wait();
 
   waveCount = await waveContract.getTotalWaves();
+
+  let waveList = await waveContract.printAddresses();
 };
 
 const runMain = async () => {
